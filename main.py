ee06b6a81126ff1ba28111247f47e269d7abb3e6
iamAxilles <axilles300bc@gmail.com>
2026-07-20 11:27:58 +0900

    port 8000 main.py

--- main.py
+++ main.py
@@ -137,8 +137,8 @@ app.mount("/", StaticFiles(directory="public2"))
 
 
 
-#if __name__ == "__main__":
-#    uvicorn.run('main:app', host="0.0.0.0", port=33, reload=True)
+if __name__ == "__main__":
+   uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
 
 
 
